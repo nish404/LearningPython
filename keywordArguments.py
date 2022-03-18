0a9876d97184a0b44aae4569278f8c6f3f1c7867
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:39:44 -0400

    Create keywordArguments.py

---
+++ keywordArguments.py
@@ -0,0 +1,8 @@
+# keyword arguments = arguments by an identifier when we pass them to a function 
+# the order of the arguments doesn't matter, unlike positional arguments 
+# python knows the names of the arguments that our function receives 
+
+def hello(first,middle,last):
+    print("hello "+first+" "+middle+" "+last)
+
+hello(last="Nish",middle="Grey",first="Code")
