c7fe1bbe04defd82ab2246c814b3b217f31cd5ed
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:17:31 -0400

    Create typeCast.py

---
+++ typeCast.py
@@ -0,0 +1,12 @@
+# type casting = convert the data type of a value to another data type
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+x = float(x)
+z = int(z)
+
+print(x)
+print("y is "+str(y))
+print(z*3)
