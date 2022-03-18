230e12e7770b507e8af3ba3359f78189e1ff1718
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 12:55:50 -0400

    Create pip.py

---
+++ pip.py
@@ -0,0 +1,12 @@
+# *******************************************************
+# python pip
+# *******************************************************
+# pip = package manager for packages and modules from python package index
+#       included for python versions 3.4+
+#       open command prompt 
+#       help:                           pip
+#       check:                          pip --version
+#       update:                         pip install --upgrade pip
+#       installed packages:             pip list
+#       check outdated packages:        pip list --outdated
+#       install a package:              pip install "package name"
