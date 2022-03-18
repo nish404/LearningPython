1d35f911abd3d40114aec0e4214d630adc4887a4
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 13:19:39 -0400

    Create runWithCommandPrompt.py

---
+++ runWithCommandPrompt.py
@@ -0,0 +1,13 @@
+# *******************************
+# run .py file with cmd
+# *******************************
+# save file as .py (python file)
+# go to command prompt; in macOS this would be the terminal
+# navigate to directory w/ your file: C:\Users\User\Desktop
+# invoke python interpreter + script: python hello_world.py
+
+print("hello world")
+
+name = input("what's your name?: ")
+
+print("hello "+name)
