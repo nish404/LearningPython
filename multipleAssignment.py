517745365faf8f8b338e7b618e821bc0a7b5a720
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:17:03 -0400

    Create multipleAssignment.py

---
+++ multipleAssignment.py
@@ -0,0 +1,23 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code 
+
+# name = "Nish"
+# age = 21
+# short = True
+
+name, age, short = "Nish", 21, True
+
+print(name)
+print(age)
+print(short)
+
+# spongebob = 30
+# patrick = 30
+# sandy = 30
+# squidward = 30
+
+spongebob = patrick = sandy = squidward = 30
+
+print(spongebob)
+print(patrick)
+print(sandy)
+print(squidward)
