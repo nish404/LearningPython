aa6c091f71a01c77f16065b68151453ecf5cf029
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 12:56:30 -0400

    Create pyToExe.py

---
+++ pyToExe.py
@@ -0,0 +1,9 @@
+#   (windows defender may prevent you from running)
+#   (make sure popo and pyinstaller are installed/updated)
+#   cd to drectory that contains your .py file
+#   pyinstaller ...
+#           -F              (all in 1 file)
+#           -w              (removes terminal window)
+#           -i icon.ico     (adds custom icon to .exe)
+#           clock.py        (name of your main python file)
+#   .exe is located in the dist folder
