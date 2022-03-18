791033e6abef538b4d06fa85d73e245179a09ce8
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:21:02 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,18 @@
+# list = used to store mltiple items in a single variable
+
+food = ["pizza","cheeseburgers","hotdogs","spaghettti","cake"]
+
+food[0] = "shrimp"
+
+#food.append("ice cream")
+#food.remove("hotdogs")
+#food.pop()
+#food.insert(1,"crabs")
+#food.sort()
+
+food.clear()
+ 
+#print(food[0]) 
+
+for x in food:
+    print(x)
