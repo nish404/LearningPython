e43fd211837729af0025379152d8d3ade5f83366
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:18:28 -0400

    Create stringSlicing.py

---
+++ stringSlicing.py
@@ -0,0 +1,17 @@
+# slicing = create a substring by extracting elements from another string 
+# indexing[] or slice()
+# [start:stop:step]
+
+#name = "Nish Code"
+#first_name = name[:4]
+#last_name = name[5:]
+#funky_name = name[::2]
+#reversed_name = name[::-1]
+
+#print(reversed_name)
+
+website = "https://google.com"
+
+slice = slice(8,-4)
+
+print(website[slice])
