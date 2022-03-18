9147b456f66913300d3da8909aa96f7da269cf69
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 12:46:48 -0400

    Create highOrderFunctions.py

---
+++ highOrderFunctions.py
@@ -0,0 +1,28 @@
+# High Order Function = a function that either:
+#                       1. accepts a function as an argument 
+#                       or
+#                       2. returns a function
+#                       (in python, functions are also treated as objects)
+
+def loud(text):
+    return text.upper()
+
+def quiet(text):
+    return text.lower()
+
+def hello(func):
+    text = func("hello")
+    print(text)
+
+hello(loud)
+hello(quiet)
+
+##########################
+
+def divisor(x):
+    def dividend(y):
+        return y / x
+    return dividend
+
+divide = divisor(2)
+print(divide(10))
