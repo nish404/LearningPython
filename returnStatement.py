62d505412cd8ad06697233cb8019e79013a9d6af
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:39:00 -0400

    Create returnStatement.py

---
+++ returnStatement.py
@@ -0,0 +1,10 @@
+# return statement = functions send python values/objects back to the caller 
+# these values/objects are known as the function's return value 
+
+def multiply(num1,num2):
+    result = num1 * num2
+    return result
+
+x = multiply(6,8)
+
+print(x)
