70997b6321a920f9efcf583bdff75215789a7721
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:21:38 -0400

    Create 2dLists.py

---
+++ 2dLists.py
@@ -0,0 +1,9 @@
+# 2D Lists = a list of lists
+
+drinks = ["tea","coffee","water"]
+dinner = ["salmon","shrimp","crabs"]
+dessert = ["cookies","cake","donuts"]
+
+food = [drinks,dinner,dessert]
+
+print(food[0][0])
