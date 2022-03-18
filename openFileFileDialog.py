82a34d23eab1e4cd467d637692d183375c2b8770
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 13:11:37 -0400

    Create openFileFileDialog.py

---
+++ openFileFileDialog.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import filedialog
+
+def openFile():
+    filepath = filedialog.askopenfilename(initialdir="C:\\Users\\Nish\\Python", 
+                                            title="open file ok?,",
+                                            typenames=(("text files","*.txt","all files","*.*")))
+    file = open(filepath,'r')
+    print(file.read())
+    file.close()
+    
+#for mac you would not include filetypes as it does not exist as an option
+
+window = Tk()
+
+button = Button(window,text='open',command=openFile)
+button.pack()
+
+window.mainloop()
