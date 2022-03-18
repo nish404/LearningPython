eb811e8860fd183ac06c44d713ed732d4f485ee1
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:15:57 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,25 @@
+# variable = a container for a value. Behaves as the value that it contains 
+
+first_name = "Nish"
+last_name = "Codes"
+full_name = first_name +" "+ last_name
+
+print("hello "+full_name)
+#print(type(name))
+
+age = 21
+age += 1
+#age = age + 1
+
+print("your age is: "+str(age))
+#print(type(age))
+
+height = 154.94
+
+print("your height is: "+str(height)+"cm")
+#print(type(height))
+
+human = True
+
+print("are you a human: "+str(human))
+#print(type(human))
