0b361146c1eefc755f32f4275cc892b99d7e0371
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:35:59 -0400

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,8 @@
+# function = a block of code which is executed only when it is called 
+
+def hello(first_name,last_name,number):
+    print("hello "+first_name+" "+last_name)
+    print("your number is "+str(number))
+    print("have a nice day")
+
+hello("Nish","Code",7)
