729bbb6154a3a9b043f7a1f7bd1fc0944edae3e1
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 10:38:40 -0400

    Create indexing.py

---
+++ indexing.py
@@ -0,0 +1,14 @@
+# index operator [] = gives access to a sequence's element (str,list,tuples)
+
+name = "nish code"
+
+#if(name[0].islower()):
+#    name = name.capitalize()
+
+first_name = name[:4].upper()
+last_name = name[5:].lower()
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
